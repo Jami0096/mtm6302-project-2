a4c0c363ac242bc1d1ad1d7a8be80285cec39b1f
Jami0096 <77336144+Jami0096@users.noreply.github.com>
2022-04-12 13:13:16 -0400

    2nd comitt

---
+++ script.js
@@ -0,0 +1,22 @@
+const data = {
+    currentUser: 'currentUser',
+    apod: [
+      {
+
+
+
+
+$form = addEventListener()
+
+$else = preventDefault()
+
+const response = await fetch ('')
+const json = await response.json()
+
+$apod.innerHTML =`
+   <div>
+      <h2>${json.title}</h2>
+</div>
+`
+)
+    
