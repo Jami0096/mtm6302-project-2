d8a61cd2f3c326dfc22f7bb55112eaedca5cf6a8
Jami0096 <77336144+Jami0096@users.noreply.github.com>
2022-04-19 13:59:49 -0400

    This Completes the project

--- script.js
+++ script.js
@@ -14,9 +14,16 @@ $btnGet.addEventListener('click',function (e){
          fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=${$date.value}&end_date=${$date.value}`)
                 .then(response=>response.json())
                 .then(json=> {
-                  const data =json[0]
-                  console.log(data)
-                  $result.innerHTML = `<img src=${data.url}>`
+                    const data =json[0]
+                    console.log(data)
+
+                    $result.innerHTML = `
+                    <img>${data.url}</img>
+                    <h3>${data.title}</h3>
+                    <p>${data.date}</p>
+                    <p>${data.explanation}</p>`
+
+
                })
                .catch(error => {
                  alert(`${error.name} - ${error.message}`)
