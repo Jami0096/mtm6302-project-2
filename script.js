476ca3bd36d3e6cbec07aa00c2984c4c74c30276
Jami0096 <77336144+Jami0096@users.noreply.github.com>
2022-04-19 13:48:01 -0400

    almost there

--- script.js
+++ script.js
@@ -1,96 +1,26 @@
-const data ={ currentUser: 'curentUser',
-i
-
 const $btnGet=document.getElementById('btn-get')
 const $result=document.getElementById('result')
 const $date=document.getElementById('date')
 
+
+
 $btnGet.addEventListener('click',function (e){
-     e.preventDefault()
-
-     fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&
-     start_date= ${$date.value}&end_date=${$date.value}`)
-      .then(response=>response.json())
-      .then(json=> {
-           const data =json[0]
-           console.log(data)
-           $result.innerHTML = `<img src=${data.url
-  }>`
-         .catch(error => {
-           alert(`${error.name} - ${error.message}`)
-         })
-  })
+    e.preventDefault()
+    if ($date.value ==''){
+      alert('no data selected')
 
 
+     } else { 
+         fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=${$date.value}&end_date=${$date.value}`)
+                .then(response=>response.json())
+                .then(json=> {
+                  const data =json[0]
+                  console.log(data)
+                  $result.innerHTML = `<img src=${data.url}>`
+               })
+               .catch(error => {
+                 alert(`${error.name} - ${error.message}`)
+               })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const data = {
-//     currentUser: 'currentUser',
-//     apod: [
-//       {
-
-
-//         $body= addEventListener()
-//         if()
-//         const hd = else.target.dataset.hd
-//         $overlay.inner.HTML = `<img src="${hd">`
-
-//     $form = addEventListener()
-
-//     $else = preventDefault()
-
-//     const response = await fetch ('')
-//     const json = await response.json()
-
-//     $apod.innerHTML =()`
-//        <div>
-//         <h2>${json.title}</h2>
-//        </div>`
-
-//     $apod= addEventListener()
-//        if favorites.push()
-  
-//        saveFavorites()
-//        BuildFavorites(
-//    }
-//   )
-
-
-
-
-
-
-
-
-// {
-//     "date":"2022-03-27",
-//     "explanation":"Why would the surface of Titan light up with a blinding flash? The reason: a sunglint from liquid seas.  Saturn's moon Titan has numerous smooth lakes of methane that, when the angle is right, reflect sunlight as if they were mirrors.  Pictured here in false-color, the robotic Cassini spacecraft that orbited Saturn from 2004 to 2017 imaged the cloud-covered Titan in 2014 in different bands of cloud-piercing infrared light.  This specular reflection was so bright it saturated one of Cassini's infrared cameras. Although the sunglint was annoying -- it was also useful.  The reflecting regions confirm that northern Titan houses a wide and complex array of seas with a geometry that indicates periods of significant evaporation.  During its numerous passes of our Solar System's most mysterious moon, Cassini has revealed Titan to be a world with active weather -- including times when it rains a liquefied version of natural gas.",
-//     "hdurl":"https://apod.nasa.gov/apod/image/2203/TitanGlint_cassini_2002.jpg",
-//     "media_type":"image",
-//     "service_version":"v1",
-//     "title":"Titan Seas Reflect Sunlight",
-//     "url":"https://apod.nasa.gov/apod/image/2203/TitanGlint_cassini_960.jpg"
-//  }
-    
+              }
+ })
